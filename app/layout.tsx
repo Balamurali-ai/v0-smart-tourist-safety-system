9a331c9c6f548c40e7ea6f390c2a398d5cc26def
Balamurali S <balamurali.s2024aids@sece.ac.in>
2025-10-11 14:56:46 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,25 +30,7 @@ export default function RootLayout({
           <Analytics />
         </Suspense>
 
-        {/* 2. Add the Script component here, before the closing </body> tag */}
-        <Script id="chatbase-embed" strategy="afterInteractive">
-          {`
-            (function(){
-              if(!window.chatbase||window.chatbase("getState")!=="initialized"){
-                  window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};
-                  window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}});
-              }
-              const onLoad=function(){
-                  const script=document.createElement("script");
-                  script.src="https://www.chatbase.co/embed.min.js";
-                  script.id="s7nauVgwXbisoOh9gDeH4";
-                  script.domain="www.chatbase.co";
-                  document.body.appendChild(script)
-              };
-              if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}
-            })();
-          `}
-        </Script>
+       
       </body>
     </html>
   )
